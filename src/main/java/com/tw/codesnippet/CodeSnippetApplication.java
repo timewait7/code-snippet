ba102e2bc6ebdf6b7340700b1500338a353d7ec3
linfeng <1029776125@qq.com>
2024-02-24 11:46:00 +0800

    first commit

---
+++ src/main/java/com/tw/codesnippet/CodeSnippetApplication.java
@@ -0,0 +1,13 @@
+package com.tw.codesnippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSnippetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tw/codesnippet/CodeSnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.codesnippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSnippetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
